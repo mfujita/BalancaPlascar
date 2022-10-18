44f8aa7706a3e56b0752d2bb148c2a9513daf3d6
Murilo Fujita <murilo.fujita@sequor.com.br>
2022-10-18 08:26:49 -0300

    Tratamento quando a entrada é incorreta.

--- BalancaPlascar/Scales.cs
+++ BalancaPlascar/Scales.cs
@@ -52,102 +52,112 @@ namespace BalancaPlascar
 
         public void SetVariables()
         {
-            if (entrada[0].Equals('A') && letras[8] == (int)13)
+            try
             {
-                tara = true;
-                movimento = true;
-                erro = false;
-                statusOk = true;
+                if (entrada[0].Equals('A') && letras[8] == (int)13)
+                {
+                    tara = true;
+                    movimento = true;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('B') && letras[8] == (int)13)
+                {
+                    tara = false;
+                    movimento = true;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('C') && letras[8] == (int)13)
+                {
+                    tara = true;
+                    movimento = true;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('D') && letras[8] == (int)13)
+                {
+                    tara = false;
+                    movimento = false;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('E') && letras[8] == (int)13)
+                {
+                    tara = true;
+                    movimento = false;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('F') && letras[8] == (int)13)
+                {
+                    tara = false;
+                    movimento = false;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('G') && letras[8] == (int)13)
+                {
+                    tara = true;
+                    movimento = false;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('H') && letras[8] == (int)13)
+                {
+                    tara = false;
+                    movimento = true;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('H') && letras[8] == (int)13)
+                {
+                    tara = false;
+                    movimento = true;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('I') && letras[8] == (int)13)
+                {
+                    tara = true;
+                    movimento = false;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('L') && letras[8] == (int)13)
+                {
+                    tara = false;
+                    movimento = false;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('M') && letras[8] == (int)13)
+                {
+                    tara = true;
+                    movimento = false;
+                    erro = false;
+                    statusOk = true;
+                }
+                else if (entrada[0].Equals('@') && letras[8] == (int)13)
+                {
+                    tara = false;
+                    movimento = true;
+                    erro = false;
+                    statusOk = true;
+                }
+                else
+                {
+                    tara = false;
+                    movimento = false;
+                    erro = true;
+                    statusOk = false;
+                }
             }
-            else if (entrada[0].Equals('B') && letras[8] == (int)13)
-            {
-                tara = false;
-                movimento = true;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('C') && letras[8] == (int)13)
-            {
-                tara = true;
-                movimento = true;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('D') && letras[8] == (int)13)
+            catch
             {
                 tara = false;
                 movimento = false;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('E') && letras[8] == (int)13)
-            {
-                tara = true;
-                movimento = false;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('F') && letras[8] == (int)13)
-            {
-                tara = false;
-                movimento = false;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('G') && letras[8] == (int)13)
-            {
-                tara = true;
-                movimento = false;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('H') && letras[8] == (int)13)
-            {
-                tara = false;
-                movimento = true;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('H') && letras[8] == (int)13)
-            {
-                tara = false;
-                movimento = true;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('I') && letras[8] == (int)13)
-            {
-                tara = true;
-                movimento = false;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('L') && letras[8] == (int)13)
-            {
-                tara = false;
-                movimento = false;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('M') && letras[8] == (int)13)
-            {
-                tara = true;
-                movimento = false;
-                erro = false;
-                statusOk = true;
-            }
-            else if (entrada[0].Equals('@') && letras[8] == (int)13)
-            {
-                tara = false;
-                movimento = true;
-                erro = false;
-                statusOk = true;
-            }
-            else
-            {
-                tara = false;
-                movimento = false;
-                erro = false;
+                erro = true;
                 statusOk = false;
             }
         }
